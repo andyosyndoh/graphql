ab20853a61689fcac9c0e6b42fd1454b3fd4773a
aosindo <andyovvo8@gmail.com>
2025-03-06 15:18:36 +0300

    chore: added adits and login section

--- templates.js
+++ templates.js
@@ -10,35 +10,36 @@ export const login = () => `
 
 export const main = () => `
     <div class="container">
-    <div class="header">
-    <h2>
-        Welcome, <span id="username" class="dropdown-toggle">Loading...</span>
-        <div class="dropdown-content" id="user-details">
-            <p><strong>Full Name:</strong> <span id="full-name">-</span></p>
-            <p><strong>Email:</strong> <span id="email">-</span></p>
-            <p><strong>Phone:</strong> <span id="phone">-</span></p>
-            <p><strong>Gender:</strong> <span id="gender">-</span></p>
-            <p><strong>Age:</strong> <span id="dob">-</span></p>
+        <div class="header">
+            <h2>
+                Welcome, <span id="username" class="dropdown-toggle">Loading...</span>
+                <div class="dropdown-content" id="user-details">
+                    <p><strong>Login:</strong> <span id="login">-</span></p>
+                    <p><strong>Full Name:</strong> <span id="full-name">-</span></p>
+                    <p><strong>Email:</strong> <span id="email">-</span></p>
+                    <p><strong>Phone:</strong> <span id="phone">-</span></p>
+                    <p><strong>Gender:</strong> <span id="gender">-</span></p>
+                    <p><strong>Age:</strong> <span id="dob">-</span></p>
+                </div>
+            </h2>
+            <button class="audits-btn" id="audits-btn">No audits</button>
+            <div class="audits-dropdown" id="audits-dropdown">
+                <!-- Audit items will be dynamically added here -->
+            </div>
+            <button class="logout-btn" id="logout-btn">Logout</button>
         </div>
-    </h2>
-    <button class="audits-btn" id="audits-btn">No audits</button>
-    <div class="audits-dropdown" id="audits-dropdown">
-        <!-- Audit items will be dynamically added here -->
-    </div>
-    <button class="logout-btn" id="logout-btn">Logout</button>
-</div>
 
-    <div class="stats">
-        <div class="stat-card">XP: <span id="xp">0</span></div>
-        <div class="stat-card">Grade: <span id="grade">0</span></div>
-        <div class="stat-card">Audit Ratio: <span id="audits">0</span></div>
-    </div>
-    <div class="graph-section">
-        <h3>Statistics</h3>
-        <div class="skills-section">
-            <h3>Top 5 Skills</h3>
-            <div id="skills-container"></div>
+        <div class="stats">
+            <div class="stat-card">XP: <span id="xp">0</span></div>
+            <div class="stat-card">Grade: <span id="grade">0</span></div>
+            <div class="stat-card">Audit Ratio: <span id="audits">0</span></div>
         </div>
-    </div>
+        <div class="graph-section">
+            <h3>Statistics</h3>
+            <div class="skills-section">
+                <h3>Top 5 Skills</h3>
+                <div id="skills-container"></div>
+            </div>
+        </div>
     </div>
 `;
